d288e6d58a0cbb05ed5b82db17b87238175ed2da
AllisonAnz <AllisonAnzalone@gmail.com>
2021-08-04 14:30:06 -0500

    Completed Assignment

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,18 @@
+#using_while puts out the phrase 'Wingardium Leviosa' while the levitation force is incremented to 10
+
 def using_while
   levitation_force = 6
 	
-  #your code here
+  while levitation_force < 10
+    puts "Wingardium Leviosa"
+    levitation_force += 1
+  end
 end
+
+using_while
+#=> Wingardium Leviosa
+#=> Wingardium Leviosa
+#=> Wingardium Leviosa
+#=> Wingardium Leviosa
 
 
